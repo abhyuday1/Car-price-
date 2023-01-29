cd63948d49eddb27d9f0cf6671d92e35ae334ca7
abhyuday1 <abhyudaykumarabl@gmail.com>
2023-01-30 00:15:34 +0530

    Adding the homepage oe the web app

--- app.py
+++ app.py
@@ -22,5 +22,15 @@ def predict_api():
     output=regmodel.predict(new_data)
     return jsonify(output[0])
 
+@app.route('/predict',methods=['POST'])
+
+def predict():
+    data=[float(x) for x in request.form.values()]
+    new_data=np.array(data).reshape(1,-1)
+    print(new_data)
+    output=regmodel.predict(new_data)
+    print(output)
+    return render_template("home.html",predicted_value="The predicted value of the car is {} in lacs".format(output))
+
 if __name__=="__main__":
     app.run(debug=True)
